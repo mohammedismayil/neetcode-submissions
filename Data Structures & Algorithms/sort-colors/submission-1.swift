41324ed73663273665ab02f24f4b5f5fe1333a00
Ismayil <30821770+mohammedismayil@users.noreply.github.com>
2026-08-05 18:43:07 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func sortColors(_ nums: inout [Int]) {
+        var dict: [Int: Int] = [:]
+        for num in nums {
+            dict[num, default: 0] += 1
+        }
+        var current = 0
+        for i in 0..<3{
+            var count = dict[i, default: 0]
+            while count != 0 {
+                 nums[current] = i
+                count -= 1
+               
+                current += 1
+                
+            }
+        }
+    }
+}
